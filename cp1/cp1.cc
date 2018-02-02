025408a344925984251f3fcee1eba387d816dd5d
xionglinlin <xionglinlin2@gmail.com>
2018-02-02 15:33:34 +0800

    complete cp1

--- cp1/cp1.cc
+++ cp1/cp1.cc
@@ -6,7 +6,7 @@
 # define BUFFERSIZE 4096
 # define COPYMODE 0644
 
-void oops(const char*,char*);
+void oops(const char*,const char*);
 
 int main(int argc,char *argv[]){
     int in_fd,out_fd,n_chars;
@@ -21,7 +21,7 @@ int main(int argc,char *argv[]){
         oops("Can't open ",argv[1]);
     }
 
-    if((out_fd = open(argv[2],O_WRONLY)) == -1){
+    if((out_fd = creat(argv[2],COPYMODE)) == -1){
         oops("Can't open ",argv[2]);
     }
 
@@ -30,10 +30,18 @@ int main(int argc,char *argv[]){
             oops("Write error to ",argv[2]);
         }
     }
+
+    if(n_chars == -1){
+        oops("Read error from ",argv[1]);
+    }
+    if(close(in_fd) == -1 || close(out_fd) == -1){
+        oops("Error closing files","");
+    }
     return 0;
 }
 
-void oops(const char * s1, const char * s2){
-    fprintf(stderr,"Error: %s %s",s1,s2);
+void oops(const char * s1,const char * s2){
+    fprintf(stderr,"Error: %s",s1);
+    perror(s2);
     exit(1);
 }
