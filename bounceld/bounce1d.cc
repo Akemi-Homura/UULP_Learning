87a25ce6dd4a4ce671e6df23bc2cc0b66865d624
quals <xionglinlin2@gmail.com>
2018-07-31 13:31:06 +0800

    完成了bounce2d

--- bounceld/bounce1d.cc
+++ bounceld/bounce1d.cc
@@ -2,12 +2,11 @@
 # include <curses.h>
 # include <signal.h>
 # include <string.h>
-# include <sys/time.h>
+# include "time_util.h"
 
 # define MESSAGE "hello"
 # define BLANK   "     "
 
-int set_ticker(int);
 
 int row;
 int col;
@@ -62,20 +61,4 @@ void move_msg(int){
     }else if( dir ==1 && col + (int)strlen(MESSAGE) >= COLS){
         dir = -1;
     }
-}
-
-int set_ticker(int n_msecs){
-    struct itimerval new_timeset;
-    long n_sec, n_usecs;
-
-    n_sec = n_msecs / 1000;
-    n_usecs = (n_msecs % 1000) * 1000L;
-
-    new_timeset.it_interval.tv_sec = n_sec;
-    new_timeset.it_interval.tv_usec = n_usecs;
-
-    new_timeset.it_value.tv_sec = n_sec;
-    new_timeset.it_value.tv_usec = n_usecs;
-
-    return setitimer(ITIMER_REAL,&new_timeset,NULL);
 }
