1b7471f9307046f7b8b4602db5ae97d2061c9a8a
quals <xionglinlin2@gmail.com>
2018-08-05 10:12:35 +0800

    complete forkdemo3

---
+++ forkdemo3/forkdemo3.cc
@@ -0,0 +1,20 @@
+# include <stdio.h>
+# include <unistd.h>
+# include <sys/types.h>
+
+int main(){
+    int fork_rv;
+
+    printf("Before: my pid is %d\n",getpid());
+
+    fork_rv = fork();
+
+    if( fork_rv == -1 ){
+        perror("fork");
+    }else if( fork_rv == 0 ){
+        printf("I am the child. my pid = %d\n",getpid());
+    }else{
+        printf("I am the father. my child is %d\n",fork_rv);
+    }
+    return 0;
+}
