c036e14e64c36850127fd25819049627f0ab62a6
quals <xionglinlin2@gmail.com>
2018-08-05 11:25:09 +0800

    complete ex8-2

---
+++ ex8-2/forkdemo3.cc
@@ -0,0 +1,14 @@
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+int main(){
+    for(int n=0;n<10;n++){
+        printf("my pid = %d, n = %d\n",getpid(),n);
+        sleep(1);
+        if(fork()!=0){
+            exit(0);
+        }
+    }
+    return 0;
+}
