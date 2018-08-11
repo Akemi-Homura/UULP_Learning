764734dd6d4fe5a6df8550d6938e08b819762e8a
xionglinlin <xionglinlin2@gmail.com>
2018-08-11 23:18:15 +0800

    complete test.sh

--- ex10-10/test.sh
+++ ex10-10/test.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
+IP=$SERVER1
+USER=$LOGINNAME1
+PORT=$PORT1
+SSHPATH=$SSHPATH1
+SSHCOMMAND="ssh $USER@$IP -p$PORT -i $SSHPATH"
 
-tmux send-keys
+ while true
+ do
+     tmux send-keys -t "^.2" "$SSHCOMMAND" Enter
+     sleep 3
+     tmux send-keys -t "^.2" C-d
+     sleep 3
+ done
