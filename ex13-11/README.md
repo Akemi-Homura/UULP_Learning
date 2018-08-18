96dfba08e911849f3a24aaea863620716f8edc94
quals <xionglinlin2@gmail.com>
2018-08-18 09:44:36 +0800

    complete ex13-11

---
+++ ex13-11/README.md
@@ -0,0 +1,7 @@
+# dgrec
+UDP服务器与客户端demo
+打印接受时间和接收编号
+
+dgrecv: 接收端
+dgsend: 发送端
+dgram:  工具函数
---
+++ ex13-11/dgrecv.cc
@@ -0,0 +1,57 @@
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/types.h>
+# include <time.h>
+# include <sys/socket.h>
+# include <netinet/in.h>
+# include "dgram.h"
+
+# define oops(msg,x) {perror(msg); exit(x);}
+
+void say_who_called(struct sockaddr_in*);
+
+int main(int ac,char** av){
+    int     port;
+    int     sock;
+    char    buf[BUFSIZ];
+    size_t  msglen;
+    struct  sockaddr_in saddr;
+    socklen_t saddrlen;
+
+    if( ac == 1 || (port = atoi(av[1])) <= 0){
+        fprintf(stderr,"usage: dgrecv portnumber\n");
+        exit(1);
+    }
+
+    /*
+     * get a socket and assign it a port number
+     */
+    if( (sock = make_dgram_server_socket(port)) == -1 ){
+        oops("cannot make socket",2);
+    }
+
+    /*
+     * receive message on that socket
+     */
+    saddrlen = sizeof(saddr);
+    while((msglen = recvfrom(sock,buf,BUFSIZ,0,(struct sockaddr*)&saddr,&saddrlen))>0){
+        buf[msglen] = '\0';
+        printf("dgrecv: got a message: %s\n",buf);
+        say_who_called(&saddr);
+    }
+    return 0;
+}
+
+void say_who_called(struct sockaddr_in *addrp){
+    static int msg_num = 0;
+    char host[BUFSIZ];
+    int port;
+
+    get_internet_address(host,BUFSIZ,&port,addrp);
+    printf("from: %s:%d\n",host,port);
+
+    time_t now = time(NULL);
+    char *timestmp = ctime(&now);
+    printf("at %s",timestmp);
+    printf("msg#: %d\n\n",msg_num++);
+}
