30a475a3a1702c65f915fe1e659d4f29af012a26
quals <xionglinlin2@gmail.com>
2018-08-21 11:02:38 +0800

    complete ex14-7

---
+++ ex14-7/README.md
@@ -0,0 +1,4 @@
+# twordcounter
+两个县城，一个计数器，一个互斥量
+互斥量使用 `pthread_mutex_t`
+修改为可以读取多个文件
---
+++ ex14-7/twordcount2.cc
@@ -0,0 +1,53 @@
+# include <stdio.h>
+# include <stdlib.h>
+# include <pthread.h>
+# include <ctype.h>
+
+int total_words;
+pthread_mutex_t counter_lock = PTHREAD_MUTEX_INITIALIZER;
+
+# define PRONAME "twordcount2"
+
+void * count_words(void *);
+
+int main(int ac,char **av){
+    pthread_t *thds;
+
+    if( ac < 2 ){
+        printf("usage: %s file ...\n",PRONAME);
+        exit(1);
+    }
+
+    int filenum = ac - 1;
+    thds = (pthread_t*)malloc(filenum * sizeof(pthread_t));
+
+    total_words = 0;
+    for(int i=0;i<filenum;i++){
+        pthread_create(&thds[i], NULL, count_words, (void*)av[i]);
+    }
+    for(int i=0;i<filenum;i++){
+        pthread_join(thds[i], NULL);
+    }
+    printf("%5d: total_words\n",total_words);
+    return 0;
+}
+
+void *count_words(void *f){
+    const char* filename = (const char*)f;
+    FILE* fp;
+    int c,prevc = '\0';
+    if((fp = fopen(filename,"r")) != NULL){
+        while( (c = getc(fp)) != EOF){
+            if( !isalnum(c) && isalnum(prevc)){
+                pthread_mutex_lock(&counter_lock);
+                total_words++;
+                pthread_mutex_unlock(&counter_lock);
+            }
+            prevc = c;
+        }
+        fclose(fp);
+    }else{
+        perror(filename);
+    }
+    return NULL;
+}
